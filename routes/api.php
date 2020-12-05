08c7a41c9de26ccae52a3127e316612eb68f8393
Alex Perera <48221852+lepompe@users.noreply.github.com>
2020-12-04 19:01:15 -0500

    se agrega xml en el controller

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/API1', 'DatoGralController@buscar_datos');
+Route::post('/API2', 'DatoGralController@xml');
